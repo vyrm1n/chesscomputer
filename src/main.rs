a989d78739e76cff0acb177cd3360006eb01cd7c
vyrm1n <patrick@vyrmin.com>
2025-04-25 17:56:42 +0200

    est
    Adds algebraic notation conversion
    
    Implements a function to convert a 0-63 index
    to algebraic notation (e.g., "a1", "h8").
    
    Also adds a main function that tests the
    conversion from index 0 to 15.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+type PiecePosition = u64;
+
+// fn bit_to_position(bit: PiecePosition) -> Result<String, String> {
+//     if bit == 0 {
+//         return Err("No piece present!".to_string());
+//     } else {
+//         let onebit_index = bit_scan(bit);
+//         return Ok(index_to_position(onebit_index));
+//     }
+// }
+
+static COL_MAP: [char; 8] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+
+fn index_to_position(index: usize) -> String {
+    let column = index % 8;
+    let row = index / 8 + 1;
+    return format!("{}{}", COL_MAP[column], row);
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    for i in 0..16 {
+        println!("{} -> {}", i, index_to_position(i));
+    }
 }
